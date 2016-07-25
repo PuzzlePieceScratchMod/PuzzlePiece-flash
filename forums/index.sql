dc3e57fa4a74ab56ce90495d3e44f527c83d6ae7
edward789121 <edward789121@users.noreply.github.com>
2016-07-25 14:17:33 -0400

    Update index.sql

--- forums/index.sql
+++ forums/index.sql
@@ -8,3 +8,11 @@ user_level  INT(8) NOT NULL,
 UNIQUE INDEX user_name_unique (user_name),
 PRIMARY KEY (user_id)
 ) TYPE=INNODB;
+CREATE TABLE categories (
+cat_id          INT(8) NOT NULL AUTO_INCREMENT,
+cat_name        VARCHAR(255) NOT NULL,
+cat_description     VARCHAR(255) NOT NULL,
+UNIQUE INDEX cat_name_unique (cat_name),
+PRIMARY KEY (cat_id)
+) TYPE=INNODB;
+
